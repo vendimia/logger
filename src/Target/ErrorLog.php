a74cdf6373628cded037640e9d3f6124d20bb89c
Oliver Etchebarne <yo@drmad.org>
2026-07-22 19:16:50 -0500

    Adds more information on exception on ErrorLog target

--- src/Target/ErrorLog.php
+++ src/Target/ErrorLog.php
@@ -19,6 +19,23 @@ class ErrorLog extends TargetAbstract implements TargetInterface
 
     public function write(string|Stringable $message, array $context = [])
     {
+        // Si hay una excepción en context, añadimos más información al mensaje
+        if (key_exists('exception', $context)) {
+            $exception = $context['exception'];
+
+            $exception_class = get_class($exception);
+
+            // Si hay un mensaje, lo añadimos al final
+            $old_message = $message;
+
+            $message = "{$exception_class}: {$exception->getMessage()} on "
+                . $exception->getFile() . ':' . $exception->getLine()
+            ;
+
+            if ($old_message) {
+                $message .= ' - ' . $message;
+            }
+        }
         error_log($this->formatter->format($message, $context));
     }
 }
